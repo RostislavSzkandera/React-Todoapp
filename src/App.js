74e4744e1818f031f7226bb4eac47775af796ea3
Rostislav Szkandera <szkandera.r@seznam.cz>
2024-02-27 23:31:26 +0100

    Change form, app - add task

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Header from "./components/Header"
 import Form from "./components/Form"
 import TodosList from "./components/TodosList"
+import { useState } from "react"
 
 const App = () => {
+  const [input, setInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  
   return (
-    <div className="font-PTSans">
+    <div className="font-PTSans max-w-[350px] text-[20px] md:max-w-[600px] min-h-[200px] bg-blue-400 rounded-md mx-auto mt-40 mb-20">
       <div className="my-4">
         <Header />
       </div>
-      <div>
-        <Form />
+      <div className="ml-4 mb-4">
+        <Form input={input} setInput={setInput} todos={todos} setTodos={setTodos} />
       </div>
       <div>
         <TodosList />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,36 @@
 
-const Form = () => {
-  return (
-    <div>
-      Form
-    </div>
-  )
-}
-
-export default Form
+const Form = ({input, setInput, todos, setTodos}) => {
+  
+    // Add task
+    const formSubmit = (e) => {
+      e.preventDefault()
+  
+      setTodos([...todos, {id: new Date().getTime(), name: input, completed: false}])
+      setInput("")
+    }
+    
+    
+      return (
+      <section>
+        <form onSubmit={formSubmit}>
+          <input 
+              className="bg-blue-800 w-[200px] md:w-[300px] rounded outline-none p-2 placeholder:text-[#e4c5c5]"
+              type="text" 
+              placeholder="Jaký je další úkol?"
+              onChange={ (e) => setInput(e.target.value)}
+              value={input}
+              required
+          />
+          <input 
+              className="bg-gray-600 rounded ml-4 p-2 cursor-pointer hover:bg-gray-700"
+              type="submit" 
+              value="Přidat"
+  
+          />
+        </form>
+      </section>
+    )
+  }
+  
+  export default Form
+  
